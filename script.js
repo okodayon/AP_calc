adc9555c5c89a1a4055835e68fca43aa6701dd45
okodayon <bjrm1290142@gn.iwasaki.ac.jp>
2020-12-09 23:41:55 +0900

    js_ver2

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+//結果の欄を取得
 let result = document.getElementById("result");
+//÷ と × 処理するためにコピーをとる
+//だけのつもりだったけどこれを実装することによってキーボードの入力を受け付けなくなった!!
+
+let res = '';
+
 
 function num(elem) {
+    //文字列を直接new Functionするため÷ と ×を演算子に変える必要がある。
+    if ('÷' == elem.value) {
+        res = res + '/';
+    } else if ('×' == elem.value) {
+        res = res + '*';
+    } else {
+        res = res + elem.value;
+    }
+    //上の欄に追加していく
     result.value = result.value + elem.value;
 }
 
 function ans() {
-    if (!result.value) {
+    //空だった場合のエラー(buttonによって入力されていない場合)
+    if (!res) {
         result.value = "error";
-    }
-    result.value = new Function("return" + result.value);
+        return;
+    } 
+    //ここでresを使うことによってボタンを押して入力されたものだけ計算される
+    result.value = new Function("return " + res)();
 }
 
 function allclear() {
-    result.value = null;
+    //空にする
+    res = result.value = '';
 }
